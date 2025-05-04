6c98372918cfee86f171c976d251029491b17bdd
materancode <materanrolo@gmail.com>
2025-05-04 19:40:51 +0100

    Se actualiza la bd de nuevo

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -10,10 +10,10 @@ CREATE TABLE usuarios (
     apellido TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     direccion TEXT NOT NULL,
-    telefono TEXT NOT NULL,
+    telefono TEXT NOT NULL
 );
 
 --Insertar usuarios
 INSERT INTO usuarios (user, contrasenia, nombre, apellido, email, direccion, telefono) VALUES
-("bellaLila","123","Lila","Gutierrez", "lilabella@gmail.com","Calle hermosa 33","585812368");
-("CarolineCute","1235","Caroline","Manson", "mansoncaro@gmail.com","Calle palermo 63","5654646");
+('bellaLila','123','Lila','Gutierrez','lilabella@gmail.com','Calle hermosa 33','585812368'),
+('CarolineCute','1235','Caroline','Manson','mansoncaro@gmail.com','Calle palermo 63','5654646');
